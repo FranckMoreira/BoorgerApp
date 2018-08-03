49eb8ed43f996a2d18b6cd8b3f69052ab6087037
Franck Moreira <franck@Zorgz-iMac.local>
2018-08-03 15:27:35 -0300

    lecture 116 - starting the burger component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
-const burger = () => {
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
+import classes from './Burger.css';
+
+const burger = (props) => {
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            <BurgerIngredient type="cheese" />
+            <BurgerIngredient type="meat" />
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
 }
+
+export default burger;
