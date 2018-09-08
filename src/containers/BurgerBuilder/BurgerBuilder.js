6a5192260d1dd19bc627c6445151a9822229ddba
Franck Moreira <franck.c.moreira@gmail.com>
2018-09-08 18:11:11 -0300

    lecture 169 - Sending a POST request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 
 const INGREDIENT_PRICES = {
@@ -82,7 +83,29 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You Continue')
+        // alert('You Continue')
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.toralPrice,
+            customer: {
+                name: 'Franck Moreira',
+                address: {
+                    street: 'lavanda',
+                    zipCode: '00045',
+                    country: 'Brazil'
+                },
+                email: 'franck@gmail.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+
+        axios.post('/orders.json', order)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     render () {
